52d033cf0fdbe247f0e6378ab7fa27af7a39359b
Palak Doshi <palak.d@simformsolutions.com>
2023-08-01 02:48:12 +0530

    project setup

---
+++ SSChatView/SSChatViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SSChatViewApp.swift
+//  SSChatView
+//
+//  Created by Palak Doshi on 13/07/23.
+//
+
+import SwiftUI
+
+@main
+struct SSChatViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
