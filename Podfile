238ca5b873ece1508cc62bc8047548333e0d4eae
Palak Doshi <palak.d@simformsolutions.com>
2024-02-23 19:42:15 +0530

    Basic project setup with R.swift, custom fonts and app icons

---
+++ Podfile
@@ -0,0 +1,10 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'SSChatView' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for SSChatView
+  pod 'R.swift', '7.2.4'
+end
---
+++ SSChatView/UI/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  SSChatView
+//
+//  Created by Palak Doshi on 13/07/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+      Text("Hello World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
