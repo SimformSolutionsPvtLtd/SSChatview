8b9e64242a068e0bb7011c0617d504a30e1fa2fc
Palak Doshi <palak.d@simformsolutions.com>
2024-04-29 10:56:45 +0530

    ✨Dismiss keyboard on drag, managed no message UI and Scroll to bottom on new added message

--- SSChatView/UI/Components/Message/MessageView.swift
+++ SSChatView/UI/Components/Message/MessageView.swift
@@ -11,6 +11,7 @@ import SwiftUI
 struct MessageView: View {
     // MARK: - Variables
     @ObservedObject var viewModel: MessageViewModel
+    @State private var scrollToBottom = false // Added state variable
 }
 
 // MARK: - Body
@@ -19,21 +20,33 @@ extension MessageView {
         VStack {
             ScrollViewReader { scrollView in
                 ScrollView {
-                    LazyVStack {
+                    if viewModel.messages.isEmpty {
+                        NoMessageView()
+                            .frame(maxWidth: .infinity, maxHeight: .infinity)
+                            .padding(.vertical, (UIScreen.main.bounds.height - 100) / 2)
+                    } else {
                         ForEach(viewModel.messages, id: \.id) { message in
                             MessageUI(currentMessage: message)
                                 .id(message.id)
                         }
-                    }
-                    .onChange(of: viewModel.messages) { _ in
-                        withAnimation {
-                            scrollView.scrollTo(viewModel.messages.last?.id, anchor: .bottom)
+                        .onChange(of: viewModel.messages) { _ in
+                            withAnimation {
+                                scrollToBottom = true // Set to true to trigger scroll to bottom
+                            }
+                        }
+                        .onAppear {
+                            withAnimation {
+                                scrollToBottom = true // Set to true to trigger scroll to bottom
+                            }
                         }
                     }
-                    .onAppear {
-                        withAnimation {
-                            scrollView.scrollTo(viewModel.messages.last?.id, anchor: .bottom)
-                        }
+                }
+                .scrollDismissesKeyboard(.immediately) // Drag to dismiss keyboard.
+                .disabled(viewModel.messages.isEmpty) // Disable scroll if there are no messages
+                .onChange(of: scrollToBottom) { _ in // Detect changes in scrollToBottom
+                    if scrollToBottom {
+                        scrollView.scrollTo(viewModel.messages.last?.id, anchor: .bottom)
+                        scrollToBottom = false // Reset to false after scrolling
                     }
                 }
             }
