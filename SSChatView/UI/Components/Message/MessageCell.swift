36e21ecc49dae6954c934f0b9f0e4d0a49bea213
Palak Doshi <palak.d@simformsolutions.com>
2024-08-06 19:10:47 +0530

    ✨Added displaying selected reaction on message logic

--- SSChatView/UI/Components/Message/MessageCell.swift
+++ SSChatView/UI/Components/Message/MessageCell.swift
@@ -11,11 +11,13 @@ import SwiftUI
 struct MessageCell: View {
 
     // MARK: - Variables
-    var currentMessage: MessageResponseModel
+    @Binding var currentMessage: MessageResponseModel
     @Binding var isBlurred: Bool
     @Binding var activeMessageID: String
     @State private var reactionState = ReactionState()
+}
 
+extension MessageCell {
     // MARK: - Body
     var body: some View {
         HStack(alignment: .bottom, spacing: MessageViewConstants.spacing) {
@@ -24,23 +26,21 @@ struct MessageCell: View {
             }
 
             ZStack {
-                // Message content view
                 messageContent
             }
-            .overlay(reactionView) // Overlay reaction view on top of message content
+            .overlay(selectedReactionView, alignment: currentMessage.isCurrentUser ? .topLeading : .topTrailing)
+            .overlay(reactionView)
             .frame(
                 width: UIScreen.main.bounds.width * 0.7,
                 alignment: currentMessage.isCurrentUser ? .trailing : .leading
-            ) // Width is 70% of screen width
+            )
         }
-        .frame(
-            maxWidth: .infinity,
-            alignment: currentMessage.isCurrentUser ? .trailing : .leading
-        ) // Ensure HStack fills the available width
-        .padding() // Padding around HStack
+        .frame(maxWidth: .infinity,
+               alignment: currentMessage.isCurrentUser ? .trailing : .leading)
+        .padding()
     }
 
-    // MARK: - MessageContent
+    // MARK: - Message Content
     private var messageContent: some View {
         Text(currentMessage.content) // Display message content
             .modifier(
@@ -61,54 +61,45 @@ struct MessageCell: View {
             .blur(radius: isBlurred ? 10 : 0) // Blur message content if needed
     }
 
-    // MARK: - ReactionView
-    private var reactionView: some View {
-        ZStack {
-            ZStack {
-                RoundedRectangle(cornerRadius: 28)
-                    .fill(Color(UIColor.tertiarySystemGroupedBackground))
-                    .frame(width: 280, height: 60)
-                    .scaleEffect(
-                        isBlurred && currentMessage.id == activeMessageID ? 1 : 0,
-                        anchor: .bottomTrailing
-                    )
-                    .animation(
-                        .interpolatingSpring(stiffness: 170, damping: 15).delay(0.05),
-                        value: isBlurred
-                    )
+    // MARK: - Selected Reaction View
+    private var selectedReactionView: some View {
+        Group {
+            if let reaction = currentMessage.reaction?.imageName {
+                ZStack {
+                    Image(reaction)
+                        .padding(8)
+                        .background(SystemColors.primaryBorder)
+                        .clipShape(Circle())
+                        .shadow(radius: 3)
+                        .offset(x: currentMessage.isCurrentUser ? -20 : 20, y: -20)
 
-                Circle()
-                    .foregroundColor(Color(UIColor.tertiarySystemGroupedBackground))
-                    .frame(width: 20, height: 20)
-                    .offset(x: currentMessage.isCurrentUser ? 40 : -50, y: 30)
+                    Circle()
+                        .foregroundColor(SystemColors.primaryBorder)
+                        .offset(x: currentMessage.isCurrentUser ? -35 : 35, y: -8)
+                        .frame(width: 12, height: 12)
 
-                Circle()
-                    .foregroundColor(Color(UIColor.tertiarySystemGroupedBackground))
-                    .frame(width: 12, height: 12)
-                    .offset(x: currentMessage.isCurrentUser ? 25 : -30, y: 45)
-
-                HStack(spacing: 30) {
-                    ForEach(ReactionType.allCases, id: \.self) { reaction in
-                        Button(action: {
-                            debugPrint(reaction.imageName) // Debugging action
-                            resetReaction() // Reset reactions on button tap
-                        }, label: {
-                            Image(reaction.imageName)
-                                .scaleEffect(
-                                    reactionState.isActive(reaction) ? 1 : 0
-                                ) // Scale reaction image based on state
-                        })
-                        .buttonStyle(PlainButtonStyle())
-                    }
+                    Circle()
+                        .foregroundColor(SystemColors.primaryBorder)
+                        .frame(width: 6, height: 6)
+                        .offset(x: currentMessage.isCurrentUser ? -45 : 45, y: -2)
                 }
             }
-            .offset(x: currentMessage.isCurrentUser ? -90 : 100, y: -80) // Adjust position based on message sender
-            .opacity(isBlurred && currentMessage.id == activeMessageID ? 1 : 0)
-            .onTapGesture {
-                resetReaction() // Reset reactions on tap
-            }
+        }
+        .opacity(isBlurred ? 0 : 1)
+        .onTapGesture {
+            activeMessageID = currentMessage.id
+            currentMessage.reaction = nil
+            resetReaction()
+        }
+    }
 
-            Text(currentMessage.content) // Display message content
+    // MARK: - Reaction View
+    private var reactionView: some View {
+        ZStack {
+            reactionViewBase
+
+            // Message content displayed over reaction view
+            Text(currentMessage.content)
                 .modifier(
                     MessageText(
                         currentMessage: currentMessage,
@@ -116,6 +107,51 @@ struct MessageCell: View {
                     )
                 )
                 .opacity(isBlurred && currentMessage.id == activeMessageID ? 1 : 0)
+        }
+    }
+
+    private var reactionViewBase: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 28)
+                .fill(Color(UIColor.tertiarySystemGroupedBackground))
+                .frame(width: 280, height: 60)
+                .scaleEffect(
+                    isBlurred && currentMessage.id == activeMessageID ? 1 : 0,
+                    anchor: .bottomTrailing
+                )
+                .animation(
+                    .interpolatingSpring(stiffness: 170, damping: 15).delay(0.05),
+                    value: isBlurred
+                )
+
+            Circle()
+                .foregroundColor(Color(UIColor.tertiarySystemGroupedBackground))
+                .frame(width: 20, height: 20)
+                .offset(x: currentMessage.isCurrentUser ? 40 : -50, y: 30)
+
+            Circle()
+                .foregroundColor(Color(UIColor.tertiarySystemGroupedBackground))
+                .frame(width: 12, height: 12)
+                .offset(x: currentMessage.isCurrentUser ? 25 : -30, y: 45)
+
+            HStack(spacing: 30) {
+                ForEach(ReactionType.allCases, id: \.self) { reaction in
+                    Button(action: {
+                        currentMessage.reaction = reaction
+                        resetReaction()
+                    }, label: {
+                        Image(reaction.imageName)
+                            .scaleEffect(
+                                reactionState.isActive(reaction) ? 1 : 0) // Scale reaction image based on state
+                    })
+                    .buttonStyle(PlainButtonStyle())
+                }
+            }
+        }
+        .offset(x: currentMessage.isCurrentUser ? -90 : 100, y: -80) // Adjust position based on message sender
+        .opacity(isBlurred && currentMessage.id == activeMessageID ? 1 : 0)
+        .onTapGesture {
+            resetReaction() // Reset reactions on tap
         }
     }
 
@@ -138,6 +174,7 @@ struct MessageCell: View {
         isBlurred = false
         activeMessageID = "" // Reset active message ID
     }
+
 }
 
 // MARK: - ReactionState
--- SSChatView/UI/Components/Message/MessageView.swift
+++ SSChatView/UI/Components/Message/MessageView.swift
@@ -11,10 +11,10 @@ import SwiftUI
 struct MessageView: View {
 
     // MARK: - Variables
-    @ObservedObject var viewModel: MessageViewModel
+    @Binding var messages: [MessageResponseModel]
     @Binding var isBlurred: Bool
-    @State var activeMessageID = ""
-    @State private var scrollToBottom = false // Added state variable
+    @State private var activeMessageID = ""
+    @State private var scrollToBottom = false
 }
 
 // MARK: - Body
@@ -23,37 +23,51 @@ extension MessageView {
         VStack {
             ScrollViewReader { scrollView in
                 ScrollView {
-                    if viewModel.messages.isEmpty {
-                        // If there are no messages, just add a spacer to occupy space
-                        Spacer().frame(maxWidth: .infinity, maxHeight: .infinity)
+                    if messages.isEmpty {
+                        Spacer()
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
                     } else {
-                        ForEach(viewModel.messages, id: \.id) { message in
-                            MessageCell(
-                                currentMessage: message,
-                                isBlurred: $isBlurred,
-                                activeMessageID: $activeMessageID
-                            )
-                            .id(message.id)
-                        }
-                        .onChange(of: viewModel.messages) { _ in
-                            withAnimation {
-                                scrollToBottom = true // Set to true to trigger scroll to bottom
-                            }
-                        }
-                        .onAppear {
-                            withAnimation {
-                                scrollToBottom = true // Set to true to trigger scroll to bottom
-                            }
-                        }
+                        messageList
                     }
                 }
                 .scrollDisabled(isBlurred)
                 .scrollDismissesKeyboard(.immediately)
-                .onChange(of: scrollToBottom) { _ in // Detect changes in scrollToBottom
+                .onChange(of: scrollToBottom) { _ in
                     if scrollToBottom {
-                        scrollView.scrollTo(viewModel.messages.last?.id, anchor: .bottom)
-                        scrollToBottom = false // Reset to false after scrolling
+                        scrollView.scrollTo(messages.last?.id, anchor: .bottom)
+                        scrollToBottom = false
                     }
+                }
+            }
+        }
+    }
+
+    // MARK: - Message List
+    private var messageList: some View {
+        VStack(alignment: .leading, spacing: 0) {
+            ForEach(messages) { message in
+                MessageCell(
+                    currentMessage: Binding(
+                        get: { message },
+                        set: { newMessage in
+                            if let index = messages.firstIndex(where: { $0.id == newMessage.id }) {
+                                messages[index] = newMessage
+                            }
+                        }
+                    ),
+                    isBlurred: $isBlurred,
+                    activeMessageID: $activeMessageID
+                )
+                .id(message.id)
+            }
+            .onChange(of: messages) { _ in
+                withAnimation {
+                    scrollToBottom = true
+                }
+            }
+            .onAppear {
+                withAnimation {
+                    scrollToBottom = true
                 }
             }
         }
